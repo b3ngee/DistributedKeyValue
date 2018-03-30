bf80c5ba0d5cd9b7a60bf331c1ebd104d9fb4a1b
Jonathan /kim <jonathan@dhcp-206-12-47-52.ubcsecure.wireless.ubc.ca>
2018-03-29 19:05:42 -0700

    ClientAPI.go

---
+++ clientAPI.go
@@ -0,0 +1,58 @@
+/*
+
+Implements the clientAPI 
+
+USAGE:
+
+go run player.go [cNode Public IP] [pNode Public IP] [pNode Private IP]
+
+*/
+
+package main
+
+import (
+	//"bufio"
+	//"fmt"
+	//"net"
+	//"net/rpc"
+	//"os"
+    //"strconv"
+    //"time"
+)
+
+
+
+// Registers with the server
+func RegisterClient(){
+
+}
+
+// Writes to a store
+func Write(){
+
+}
+
+// Reads from a store
+func Read(){
+
+}
+
+//Updates the map for the client
+func UpdateStores(){
+
+}
+
+
+
+
+
+// runs the main function for the player
+func main() {
+
+}
+
+func HandleError(err error) {
+	if err != nil {
+		fmt.Println(err)
+	}
+}
