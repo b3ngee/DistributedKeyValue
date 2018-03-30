cfe641b0bb9a4daa1b923d0c8100680c8e6639c3
Yoony <p7x8@ugrad.cs.ubc.ca>
2018-03-29 21:45:59 -0700

    template for store methods

--- store.go
+++ store.go
@@ -1,8 +1,11 @@
 package main
 
-import {
+import (
+	"net"
+	"net/rpc"
+	"os"
 	"./structs"
-}
+)
 
 ///////////////////////////////////////////
 //			  Global Variables		     //
@@ -11,6 +14,44 @@ import {
 // Map of all stores in the network
 var storeNetwork map[int]
 
-func main() {
+type Store int
 
+var ServerAddress string
+var StorePublicAddress string
+var StorePrivateAddress string
+
+func ReceiveHeartBeat() {
+
+}
+
+func Log() {
+
+}
+
+func ConnectWithNetwork() {
+
+}
+
+func RegisterWithServer() {
+	// Make RPC to server
+
+}
+
+// Run store: go run store.go [PublicServerIP:Port] [PublicStoreIP:Port] [PrivateStoreIP:Port]
+func main() {
+	l := new(Store)
+	rpc.Register(l)
+
+	ServerAddress = os.Args[1]
+	StorePublicAddress = os.Args[2]
+	StorePrivateAddress = os.Args[3]
+
+	RegisterWithServer()
+
+	lis, _ := net.Listen("tcp", ServerAddress)
+
+	for {
+		conn, _ := lis.Accept()
+		go rpc.ServeConn(conn)
+	}
 }
