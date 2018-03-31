ee6ee339cfaa46efa92f5bdd93e1424f98032a74
b3ngee <bg.92@live.com>
2018-03-31 13:47:45 -0700

    Fixed merge conflicts

--- clientApp.go
+++ clientApp.go
@@ -2,32 +2,60 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"os"
 	"time"
 
 	"./clientLib"
+	"./structs"
 )
 
 var serverPubIP string
 var clientPubIP string
 var clientPrivIP string
+var stores []structs.StoreInfo
 
 func main() {
 	serverPubIP = os.Args[1]
 	clientPubIP = os.Args[2]
 	clientPrivIP = os.Args[3]
 
-	client, _ := clientLib.ConnectToServer(serverPubIP, clientPubIP)
+	userClient, storeNetwork, _ := clientLib.ConnectToServer(serverPubIP, clientPubIP)
+	stores = storeNetwork
 
-	leaderStore := "127.0.0.1:8001"
-	client.Write(5, "HELLO WORLD", leaderStore)
+	fmt.Println("Stores: ", stores)
 
-	time.Sleep(2 * time.Second)
+	address := RandomStoreAddress()
 
-	v1, e1 := client.FastRead(5)
-	fmt.Println("FastRead: ", v1, e1)
-	v2, e2 := client.DefaultRead(5)
-	fmt.Println("DefaultRead: ", v2, e2)
-	v3, e3 := client.ConsistentRead(5)
-	fmt.Println("ConsistentRead: ", v3, e3)
+	fmt.Println("address: ", address)
+
+	userClient.Write(5, "HELLO WORLD", address)
+
+	v1, e1 := userClient.FastRead(address, 5)
+	fmt.Println("first: ", v1, e1)
+	v2, e2 := userClient.DefaultRead(address, 5)
+	fmt.Println("second: ", v2, e2)
+	v3, e3 := userClient.ConsistentRead(address, 5)
+	fmt.Println("third: ", v3, e3)
+}
+
+///////////////////////////////////////////
+//	        DUPLICATE FOR EACH APP		 //
+///////////////////////////////////////////
+//			   Helpers for App			 //
+///////////////////////////////////////////
+//	        DUPLICATE FOR EACH APP		 //
+///////////////////////////////////////////
+
+// Select a random store address from a list of stores
+func RandomStoreAddress() string {
+	randomIndex := random(0, len(stores))
+	return stores[randomIndex].Address
+}
+
+// returns a random number from a range of [min, max]
+func random(min, max int) int {
+	source := rand.NewSource(time.Now().UnixNano())
+	newRand := rand.New(source)
+	return newRand.Intn(max-min) + min
 }
